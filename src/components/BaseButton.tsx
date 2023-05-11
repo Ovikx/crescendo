34d735325dedca9cebae8d5d50d2cb76711d1f8a
Ovikx <ovikdas10@gmail.com>
2023-05-10 23:18:54 -0400

    add screens

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -2,7 +2,7 @@ import { Pressable, Text } from 'react-native';
 
 export default function Button() {
     return (
-        <Pressable className='rounded-lg bg-blue-300'>
+        <Pressable className='rounded-lg bg-teal-400'>
             <Text>PRESS ME</Text>
         </Pressable>
     )
---
+++ src/pages/PracticeList.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export const PracticeList = () => {
+    return (
+        <View>
+            <Text>This is your practice list!</Text>
+        </View>
+    );
+}
