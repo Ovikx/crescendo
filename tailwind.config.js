4f045a03aad6808e8ee93c0f5640fb7162571775
Ovikx <ovikdas10@gmail.com>
2023-05-12 02:21:48 -0400

    add component dir to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
