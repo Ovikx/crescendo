0b1156cadc7cbd1fd6d699c84522df206d6f6d89
Ovikx <ovikdas10@gmail.com>
2023-05-24 21:06:17 -0400

    add top bar

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,22 @@
+import { SafeAreaView, View } from "react-native";
+import { Icon } from '../../assets/Images';
+import { Text } from 'react-native';
+
+interface Props {
+    label: string;
+    children?: string | JSX.Element | JSX.Element[];
+}
+
+export const TopBar = ({children, label} : Props) => {
+    return (
+    <SafeAreaView className='bg-gray-800 h-24'>
+        <SafeAreaView className='relative items-center'>
+            <SafeAreaView className='absolute left-4'>
+                <Icon name='menu' color='white' size={32}/>
+            </SafeAreaView>
+            <Text className='text-white font-bold mx-auto top-2 text-xl'>{label}</Text>
+        </SafeAreaView>
+        {children}
+    </SafeAreaView>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Dimensions, SafeAreaView, Text } from "react-native"
-import { OverviewStats as OverviewStats } from '../components/OverviewStats';
+import { Dimensions, SafeAreaView, Text, View } from "react-native"
+import { OverviewStats } from '../components/OverviewStats';
 import { RecentList } from '../components/RecentList';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { NavBar } from '../components/NavBar';
 import { RootStackParamList } from '../navigation/types';
+import { TopBar } from '../components/TopBar';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
@@ -11,14 +12,18 @@ export const Home = ({route, navigation}: Props) => {
     const screenSize = Dimensions.get('window').height;
 
     return (
-        <SafeAreaView className="bg-gray-900 flex-1">
-            <SafeAreaView className=''>
-                <OverviewStats textStyle='mt-16'/>
-                <RecentList textStyle='mt-6' />
+        <View className="bg-gray-900 flex-1">
+            <TopBar label='Home'/>
+            <SafeAreaView className='flex-1'>
+                
+                <SafeAreaView className=''>
+                    <OverviewStats textStyle='mt-10'/>
+                    <RecentList textStyle='mt-6' />
+                </SafeAreaView>
+                <SafeAreaView className='bottom-0 absolute'>
+                    <NavBar navigation={navigation}/>
+                </SafeAreaView>
             </SafeAreaView>
-            <SafeAreaView className='bottom-0 absolute'>
-                <NavBar navigation={navigation}/>
-            </SafeAreaView>
-        </SafeAreaView>
+        </View>
     );
 }
