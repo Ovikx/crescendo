dbf90dc125d11086a28c435d738551534deab940
Ovikx <ovikdas10@gmail.com>
2023-05-24 22:39:37 -0400

    flexible top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 export const TopBar = ({children, label} : Props) => {
     return (
-    <SafeAreaView className='bg-gray-800 h-24'>
+    <SafeAreaView className='bg-gray-800'>
         <SafeAreaView className='relative items-center'>
-            <SafeAreaView className='absolute left-4'>
+            <SafeAreaView className='absolute left-4 top-2'>
                 <Icon name='menu' color='white' size={32}/>
             </SafeAreaView>
-            <Text className='text-white font-bold mx-auto top-2 text-xl'>{label}</Text>
+            <Text className='text-white font-bold mx-auto top-2 text-xl pb-6'>{label}</Text>
         </SafeAreaView>
         {children}
     </SafeAreaView>
