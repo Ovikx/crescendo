5af2bd7a1aea29fb201ee433a7c81d90670ef007
Ovikx <ovikdas10@gmail.com>
2023-06-05 20:56:02 -0400

    initial firebase setup

---
+++ firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKS3tFVNdXnGP6aui4krBvwiQOjDOP1yQ",
+  authDomain: "crescendo-e1f2a.firebaseapp.com",
+  projectId: "crescendo-e1f2a",
+  storageBucket: "crescendo-e1f2a.appspot.com",
+  messagingSenderId: "103177151750",
+  appId: "1:103177151750:web:cf14f4f3cfb160edceea87",
+  measurementId: "G-SRVB90ZPH7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
