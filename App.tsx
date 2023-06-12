9b6e644427ea8c0d66068c783890b7766315d609
Ovikx <ovikdas10@gmail.com>
2023-06-12 02:32:36 -0400

    change field

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   DB.transaction( tx => {
     tx.executeSql(`
       CREATE TABLE IF NOT EXISTS items
-      (name TEXT, seconds INTEGER)
+      (name TEXT, mastery INTEGER)
     `)
   });
   
