40c0f89d5968b54eba355895a5735993091eb58a
Ovikx <ovikdas10@gmail.com>
2023-06-12 11:32:47 -0400

    add state for new item modal

--- src/components/NewItemModal.tsx
+++ src/components/NewItemModal.tsx
@@ -10,16 +10,28 @@ interface Props {
 }
 
 export const NewItemModal = (props: Props) => {
+    const onCancelPress = () => {
+        props.setVisible(false);
+        setInputText('');
+    };
+
+    const onSubmitPress = () => {
+        if (inputText.length > 0) {
+            props.setVisible(false);
+            console.log(inputText);
+        }
+    }
+    const [inputText, setInputText] = useState('');
+    
     return (
         <Modal
-            className='flex'
+            className='flex shadow-2xl'
             animationType='fade'
             transparent={true}
             visible={props.visible}
-
-            
             onRequestClose={() => {
-                props.setVisible(false)
+                props.setVisible(false);
+                setInputText('');
             }}
             >
             <SafeAreaView className='bg-black opacity-50 z-10 h-screen w-screen absolute' />
@@ -29,6 +41,7 @@ export const NewItemModal = (props: Props) => {
                     <SafeAreaView className='absolute top-4 right-4 p-5'>
                         <Pressable onPress={() => {
                             props.setVisible(false);
+                            setInputText('');
                         }}>
                             <Icon name='close' color='white' size={32} />
                         </Pressable>
@@ -36,23 +49,25 @@ export const NewItemModal = (props: Props) => {
                     <SafeAreaView className='mt-10 flex flex-col items-center text-center'>
                         <Text className='text-white text-2xl font-bold w-5/6 text-center'>What do you want to practice?</Text>
                         <SafeAreaView className='mx-auto'>
-                            <SafeAreaView className='rounded-full bg-gray-700 flex-row h-10 p-2 w-10/12 items-center my-6'>
+                            <SafeAreaView className='rounded-xl bg-gray-700 flex-row h-14 p-2 w-10/12 items-center my-6'>
                                 <TextInput
-                                    className='h-8 flex-1 ml-4 grow'
+                                    clearButtonMode='unless-editing'
+                                    className='h-full flex-1 ml-4 mr-2 grow text-white'
                                     placeholder='e.g. "Sight reading", "Fur Elise", etc.'
-                                    placeholderTextColor='#aaaaaa'
+                                    placeholderTextColor='#94a3b8'
+                                    onChangeText={(text) => setInputText(text)}
                                 />
                             </SafeAreaView>
                         </SafeAreaView>
                         <SafeAreaView className='mb-5 flex flex-row justify-between'>
-                            <SafeAreaView className='mx-5'>
-                                <BaseButton color='bg-slate-900' onPress={() => {}}>
-                                    <Text className='text-slate-400 font-md text-lg px-6 py-3'>Cancel</Text>
+                            <SafeAreaView className='mx-3'>
+                                <BaseButton color='bg-slate-900' onPress={onCancelPress}>
+                                    <Text className='text-slate-400 font-md text-lg px-10 py-3'>Cancel</Text>
                                 </BaseButton>
                             </SafeAreaView>
-                            <SafeAreaView className='mx-5'>
-                                <BaseButton color='bg-slate-700' onPress={() => {}}>
-                                    <Text className='text-white font-md text-lg px-6 py-3'>Practice</Text>
+                            <SafeAreaView className='mx-3'>
+                                <BaseButton color='bg-slate-700' onPress={onSubmitPress}>
+                                    <Text className='text-white font-md text-lg px-10 py-3'>Practice</Text>
                                 </BaseButton>
                             </SafeAreaView>
                         </SafeAreaView>
