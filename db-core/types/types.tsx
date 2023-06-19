710ae7ff7f64ef516e0db76e0aaae273de3b4545
Ovikx <ovikdas10@gmail.com>
2023-06-19 14:02:26 -0400

    docs for db-core

--- db-core/types/types.tsx
+++ db-core/types/types.tsx
@@ -1,6 +1,19 @@
+import { PracticeItem } from '../../src/types/types';
+
+/** Enum to represent the various SQL data types */
 export enum ColumnTypes {
-    INTEGER = 'INTEGER',
-    TEXT = 'TEXT'
+    Text = 'TEXT',
+    Integer = 'INTEGER',
+    Real = 'REAL',
+    Blob = 'BLOB',
 }
 
-export type Table<T> = {[k in keyof T]: ColumnTypes};
+export type Columns<T> = {[k in keyof T]: ColumnTypes};
+
+/** An object that represents a single table. Stores the table's name and the SQL data types of each column */
+export interface Table<T> {
+    /** The name of the table */
+    tableName: string;
+    /** Object literal mapping column names to their respective SQL data types */
+    columns: Columns<T>;
+}
