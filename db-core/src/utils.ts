866f0e5944423c5c3fb94f3a3e1f9ba9983aac5a
Ovikx <ovikdas10@gmail.com>
2023-06-22 17:59:11 -0400

    add sql tag

--- db-core/src/utils.ts
+++ db-core/src/utils.ts
@@ -1,2 +1,16 @@
 import { Columns } from '../types/types';
 import { Table } from './table';
+
+/**
+ * Tag to ensure that arbitrary migration SQL statements are formatted correctly during the migration process
+ * @param strs String to validate
+ * @returns Migration-ready SQL statement
+ */
+export function sql(strs: TemplateStringsArray, ...values: any[]) {
+    let res = '';
+    strs.forEach((str, i) => {
+        res += str + (values[i] != undefined ? values[i] : '');
+    });
+    const trimmed = res.trim();
+    return trimmed.slice(-1) == ';' ? trimmed : trimmed + ';';
+}
