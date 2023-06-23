38ee1804065d872dca5120280f92dcf005a025cd
Ovikx <ovikdas10@gmail.com>
2023-06-22 21:02:26 -0400

    add typescript-eslint

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 import { SafeAreaView, View } from "react-native";
 import { Icon } from '../../assets/Images';
-import { Text } from 'react-native';
+import { Text, Pressable } from 'react-native';
 
 interface Props {
     label: string;
@@ -12,7 +12,9 @@ export const TopBar = ({children, label} : Props) => {
     <SafeAreaView className='bg-slate-800 shadow-sm shadow-black'>
         <SafeAreaView className='relative items-center'>
             <SafeAreaView className='absolute left-4 top-2'>
-                <Icon name='menu' color='white' size={32}/>
+                <Pressable>
+                    <Icon name='menu' color='white' size={32}/>
+                </Pressable>
             </SafeAreaView>
             <Text className='text-white font-bold mx-auto top-2 text-xl pb-6'>{label}</Text>
         </SafeAreaView>
