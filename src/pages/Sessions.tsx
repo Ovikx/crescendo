e13ca26ec9b1871d645380e9c7e8629e00caccd0
Ovikx <ovikdas10@gmail.com>
2023-07-03 10:57:46 -0400

    base sessions state

--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -2,10 +2,25 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/types';
 import { View } from 'react-native';
 import { TopBar } from '../components/TopBar';
+import { useState, useEffect } from 'react';
+import { PracticeSession } from '../types/types';
+import { DB } from '../db/db';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Sessions'>;
 
 export const Sessions = ({navigation}: Props) => {
+    const [sessions, setSessions] = useState<PracticeSession[]>([]);
+    useEffect(() => {
+        DB.sessionsTable.select({
+            orderBy: [
+                {column: 'timeStarted', ascending: false},
+                {column: 'itemName', ascending: false}
+            ]
+        },
+        setSessions);
+    }, []);
+    console.log(sessions);
+
     return (
         <View className='bg-slate-900 flex-1'>
             <TopBar label='Previous Sessions'/>
